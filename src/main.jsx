5283573b90f80c9aafc0ffd1f846f81dfbd41143
morabessoneorg <mora@setandforget.io>
2023-10-02 10:48:53 -0300

    changed client id

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <GoogleOAuthProvider clientId="221017514620-u92vs7m1sc2n294j2pv2s1i00gnm77jm.apps.googleusercontent.com" scopes={['email']}>
+  <GoogleOAuthProvider clientId="92620762245-482esebrakdl68qsoo8dfc0fm1k2qlm5.apps.googleusercontent.com" scopes={['email']}>
     <React.StrictMode>
         <App />
     </React.StrictMode>
